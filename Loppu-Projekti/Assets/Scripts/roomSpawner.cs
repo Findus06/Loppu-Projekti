9360c510ed4d3343908b05dcc7e780527cd2d5c1
Findus06 <vilirask06@gmail.com>
2023-03-11 14:41:03 +0200

    Changes

--- Loppu-Projekti/Assets/Scripts/roomSpawner.cs
+++ Loppu-Projekti/Assets/Scripts/roomSpawner.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class roomSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public int openingDirection;
+  // 1 --> Tarvitsee ala ovi
+  // 2 --> Tarvitsee ylä ovi
+  // 3 --> Tarvitsee vasen ovi
+  // 4 --> Tarvitsee oikea ovi
 
-    // Update is called once per frame
-    void Update()
+ private RoomTemplates templates;
+ private int rand;
+
+  void start()
+  {
+    templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+  }
+
+  void Update()
+  {
+     if(openingDirection == 1)
     {
-        
+        rand = Random.Range(0, templates.bottomRooms.Length);
+        Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
     }
+     else if(openingDirection == 2)
+     {
+        
+     }
+     else if(openingDirection == 3)
+     {
+
+     }
+     else if(openingDirection == 4)
+     {
+
+     }
+        
+     
+  }
+
 }
