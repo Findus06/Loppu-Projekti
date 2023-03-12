a04140e9259236b56f225dcd699a818014454d29
Findus06 <vilirask06@gmail.com>
2023-03-12 17:24:03 +0200

    Update roomSpawner.cs

--- Loppu-Projekti/Assets/Scripts/roomSpawner.cs
+++ Loppu-Projekti/Assets/Scripts/roomSpawner.cs
@@ -22,35 +22,39 @@ public class roomSpawner : MonoBehaviour
 
   void Spawn()
   {
-    if (spawned == false)
-    {
-      if(openingDirection == 1)
-    {
-        rand = Random.Range(0, templates.bottomRooms.Length);
-        Instantiate(templates.bottomRooms[rand], transform.position, Quaternion.identity);
-    }
-     else if(openingDirection == 2)
-     {
-        rand = Random.Range(0, templates.topRooms.Length);
-        Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
-     }
-     else if(openingDirection == 3)
-     {
-        rand = Random.Range(0, templates.leftRooms.Length);
-        Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
-     }
-     else if(openingDirection == 4)
-     {
-        rand = Random.Range(0, templates.rightRooms.Length);
-        Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
-     }
-     spawned = true;
-    }
-
-     
-    
-        
-     
+      if (spawned == false)
+      {
+        if(openingDirection == 1)
+        {
+            rand = Random.Range(0, templates.bottomRooms.Length);
+            Instantiate(templates.bottomRooms[rand], transform.position, Quaternion.identity);
+        }
+      else if(openingDirection == 2)
+      {
+          rand = Random.Range(0, templates.topRooms.Length);
+          Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
+      }
+      else if(openingDirection == 3)
+      {
+          rand = Random.Range(0, templates.leftRooms.Length);
+          Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
+      }
+      else if(openingDirection == 4)
+      {
+          rand = Random.Range(0, templates.rightRooms.Length);
+          Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
+      }
+      spawned = true;
+      }
   }
+
+  void OnTriggerEnter2d(Collider2D other)
+  {
+    if(other.CompareTag("Spawnpoint") && other.GetComponent<roomSpawner>().spawned == true)
+    {
+      Destroy(gameObject);
+    }
+  }
+
 
 }
